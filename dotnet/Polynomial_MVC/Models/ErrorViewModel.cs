0ca6827a4220f5ed74822723683a4135ae9a10d4
Krzysztof Kulka <krzysztof.kulka1234@gmail.com>
2024-11-26 23:29:23 +0100

    adding mvc program

---
+++ dotnet/Polynomial_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Polynomial_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
